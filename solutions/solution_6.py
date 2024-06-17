62cd64617b788dc4c1e8f204b30afdf4420f0831
ed4winho <gojijoseph1@gmail.com>
2024-06-17 11:38:10 +0100

    Update solution_6.py

--- solutions/solution_6.py
+++ solutions/solution_6.py
@@ -1,9 +1,6 @@
-# Read the input
-a = int(input())
-b = int(input())
-
-# Check if a is divisible by b
-divisible = "YES" * (a % b == 0) or "NO"
-
-# Print the result
-print(divisible)
+numA=int(input("enter a number:"))
+numB=int(input("enter anothe number:"))
+if numA%numB==0:
+    print("True")
+else:
+    print("False")
