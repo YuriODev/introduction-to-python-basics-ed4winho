bb0fd5df34c9ed37b54298b4b17eb6a0421fc264
ed4winho <gojijoseph1@gmail.com>
2024-06-17 11:42:55 +0100

    Update solution_10.py

--- solutions/solution_10.py
+++ solutions/solution_10.py
@@ -1,8 +1,7 @@
-# Read the input
-a = float(input())
+def calculate_minute_hand_angle(hour_hand_angle):
+  minute_hand_angle = (hour_hand_angle % 30) * 12
+  return minute_hand_angle
+hour_hand_angle = float(input("Enter the angle by which the hour hand has turned since the beginning of the day: "))
+minute_hand_angle = calculate_minute_hand_angle(hour_hand_angle)
+print("The angle by which the minute hand has turned since the beginning of the last hour is:", minute_hand_angle)
 
-# Calculate the angle
-angle = (a % 30) * 12
-
-# Print the angle
-print(angle)
